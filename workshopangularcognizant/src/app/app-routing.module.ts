0204c3f470ddd750d174b789c93707e0e13ca0b5
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2023-06-04 14:38:55 -0300

    novo post

--- workshopangularcognizant/src/app/app-routing.module.ts
+++ workshopangularcognizant/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
 import { AppComponent } from './app.component';
 import { PostEditComponent } from './post-edit/post-edit.component';
+import { PostNewComponent } from './post-new/post-new.component';
 
 const routes: Routes = [
   //{ path: '', component: AppComponent },
   { path: 'post-list', component: PostListComponent },
-  { path: 'post-edit', component: PostEditComponent }
+  { path: 'post-edit', component: PostEditComponent },
+  { path: 'post-new', component: PostNewComponent },
 ];
 
 
